0f0d003e72e1f17a3fab8bee1620f311cfa6da43
Júlíus Helgi Júlíusson <juliohelgi@gmail.com>
2019-12-02 14:47:39 +0000

    airplane upadeted

--- Classes/airplane.py
+++ Classes/airplane.py
@@ -1,6 +1,6 @@
 class airplane():
 
-    def __init__(self, registration_number, model, active = 1, available, pilot):
+    def __init__(self, registration_number, model, active = 1,):
         ''' Active = 1, inactive = 0 '''
             self.registration_number = registration_number
             self.model = model
@@ -8,6 +8,16 @@ class airplane():
             
     def __str__(self):
         return "{},{},{}".format(self.model, self.registration_number, self.active,)
+
+    def get_registration_number(self):
+        return self.registration_number
+
+    def get_model(self):
+        return self.model
+
+    def get_active(self):
+        return self.active
+
     
 
 airplane1 = airplane("a203", 10000, 250, 1, "yes", "hans")
