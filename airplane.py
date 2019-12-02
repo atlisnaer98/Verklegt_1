2c0ae13c9c47d64c49e05a43f1f1091e9156cf51
Júlíus Helgi Júlíusson <juliohelgi@gmail.com>
2019-12-02 10:06:52 +0000

    bæta við

---
+++ airplane.py
@@ -0,0 +1,10 @@
+class airplane():
+
+    def __init__(self, model, registration_number, seat_amount, active, available, pilot):
+            self.model = model
+            self.registration_number = registration_number
+            self.seat_amount = seat_amount
+            self.active = active
+            self.available = available
+            self.pilot = pilot
+            
---
+++ destination.py
@@ -0,0 +1,10 @@
+class destination():
+
+    def __init__(self, country, airport, flight_time, distance, name_of_contact, emergency_phone_number,  ):
+        self.country = country
+        self.airport = airport
+        self.flight_time = flight_time
+        self.distance = distance
+        self.name_of_contact = name_of_contact
+        self.emergency_phone_number = emergency_phone_number
+        
