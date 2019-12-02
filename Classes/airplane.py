48647a25a34126589a1c631fd37dc63cd6eade17
Júlíus Helgi Júlíusson <juliohelgi@gmail.com>
2019-12-02 15:03:17 +0000

    update - voyage and airplane

--- Classes/airplane.py
+++ Classes/airplane.py
@@ -2,9 +2,9 @@ class airplane():
 
     def __init__(self, registration_number, model, active = 1,):
         ''' Active = 1, inactive = 0 '''
-            self.registration_number = registration_number
-            self.model = model
-            self.active = active
+        self.registration_number = registration_number
+        self.model = model
+        self.active = active
             
     def __str__(self):
         return "{},{},{}".format(self.model, self.registration_number, self.active,)
@@ -20,5 +20,5 @@ class airplane():
 
     
 
-airplane1 = airplane("a203", 10000, 250, 1, "yes", "hans")
+airplane1 = airplane("a203", 10000, 250,)
 print(airplane1) 
