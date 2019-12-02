927c6ee78dab7e8b31041fe8d7daee6712ca0125
Júlíus Helgi Júlíusson <juliohelgi@gmail.com>
2019-12-02 14:35:31 +0000

    Update - airplane

--- airplane.py
+++ airplane.py
@@ -1,14 +1,13 @@
 class airplane():
 
-    def __init__(self, model, registration_number, active, available, pilot):
-            self.model = model
+    def __init__(self, registration_number, model, active = 1, available, pilot):
+        ''' Active = 1, inactive = 0 '''
             self.registration_number = registration_number
+            self.model = model
             self.active = active
-            self.available = available
-            self.pilot = pilot
             
     def __str__(self):
-        return "{},{},{},{},{}".format(self.model, self.registration_number, self.active, self.available, self.pilot)
+        return "{},{},{}".format(self.model, self.registration_number, self.active,)
     
 
 airplane1 = airplane("a203", 10000, 250, 1, "yes", "hans")
