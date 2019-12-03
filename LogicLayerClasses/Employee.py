d9aab27526f0038379c11b3cf2f3565b92926127
Gussii <gudjon16@ru.is>
2019-12-03 12:02:35 +0000

    Gera Priv

--- LogicLayerClasses/Employee.py
+++ LogicLayerClasses/Employee.py
@@ -2,47 +2,47 @@ class Employee():
     
     def __init__(self, ID_number, name, address, home_phone, mobile_number, email_address, job_title, rank, licence, active):
         #ssn,name,address, homephone, mobilephone, email_address,job_title,rank,licence, active
-        self.ID_number = ID_number
-        self.name = name
-        self.address = address
-        self.home_phone = home_phone
-        self.mobile_number = mobile_number
-        self.email_address = email_address
-        self.job_title = job_title
-        self.rank = rank
-        self.licence = licence
-        self.active = active
+        self.__ID_number = ID_number
+        self.__name = name
+        self.__address = address
+        self.__home_phone = home_phone
+        self.__mobile_number = mobile_number
+        self.__email_address = email_address
+        self.__job_title = job_title
+        self.__rank = rank
+        self.__licence = licence
+        self.__active = active
 
 
     def __str__(self):
-        return "{},{},{},{},{},{},{},{},{},{}".format(self.ID_number, self.name, self.address, self.home_phone,self.mobile_number, self.email_address, self.job_title, self.rank, self.licence, self.active)
+        return "{},{},{},{},{},{},{},{},{},{}".format(self.__ID_number, self.__name, self.__address, self.__home_phone,self.__mobile_number, self.__email_address, self.__job_title, self.__rank, self.__licence, self.__active)
 
     def get_name(self):
-        return self.name
+        return self.__name
 
     def get_ID_number(self):
-        return self.ID_number
+        return self.__ID_number
 
     def get_address(self):
-        return self.address
+        return self.__address
     
     def get_home_phone(self):
-        return self.home_phone
+        return self.__home_phone
 
     def get_mobile_number(self):
-        return self.mobile_number
+        return self.__mobile_number
 
     def get_email_address(self):
-        return self.email_address
+        return self.__email_address
     
     def get_job_title(self):
-        return self.job_title
+        return self.__job_title
     
     def get_rank(self):
-        return self.rank
+        return self.__rank
     
     def get_licence(self):
-        return self.licence
+        return self.__licence
     
     def get_activity(self):
-        return self.active
+        return self.__active
