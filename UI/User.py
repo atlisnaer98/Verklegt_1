39b22611f10f253b0371e4206d100941c96fbf95
Gisli Thor <gislig18@ru.is>
2019-12-03 15:43:15 +0000

    Add_dest

--- UI/User.py
+++ UI/User.py
@@ -4,3 +4,11 @@ class User:
 
     def start(self):
         pass
+
+    def add_destination(self):
+        country = input("Country: ")
+        airport = input("Airport: ")
+        flight_time = input("Time of flight: ")
+        name_of_contact = input("Emergency contact: ")
+        emergency_phone_number = input("Emergency contact number: ")
+        return country, airport, flight_time, name_of_contact, emergency_phone_number
