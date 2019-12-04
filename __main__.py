f03d47c15cee8eba5193ec3d52daf22515474904
Gisli Thor <gislig18@ru.is>
2019-12-04 14:05:16 +0000

    Cabin

--- __main__.py
+++ __main__.py
@@ -3,9 +3,9 @@ from UI.User import User
 def main():
     ui = User()
     #ui.get_all_airplane()
-    ui.get_all_dest()
+    #ui.get_all_dest()
     #ui.get_all_employee()
-    #ui.get_cabin_crew()
+    ui.get_cabin_crew()
     #ui.add_dest()
     
 
