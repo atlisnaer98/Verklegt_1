420268c50e015af18c9d9fdfee1bf528fd2be6ea
Júlíus Helgi Júlíusson <juliohelgi@gmail.com>
2019-12-04 16:23:52 +0000

    appearance created

---
+++ UI/appearance.py
@@ -0,0 +1,34 @@
+# DASH = "-"
+# hastag = "#"
+
+
+# Role = "Select a Role"
+# option = "Select an option"
+# M = "Menu"
+# D = "Destination"
+# C = 'Create'
+# CH = 'Change info'
+# CV = 'Change voyage'
+# A = 'Airplanes'
+# AC = 'Assign crew'
+# E = "Employees"
+# V = "Voyage"
+# S = "Show schedule "
+# L = "List employee"
+# LD = 'List destination'
+# LV = 'list voyage'
+
+
+# B = "Back"
+# Q = "Quit"
+# empty = ''
+
+
+# print(DASH*52)
+# print("{:>26} ".format(M))
+# print(DASH*52)
+# print("{} {:<26}{} {} ".format('[1]', E, '[2]', V))
+# print("{} {:<26}{} {} ".format('[3]', D, '[4]', L))
+# print("{:<16} {}".format(empty, DASH*24))
+# print("{:<18} {} {} {} {} ".format(empty, '[B]',B, '[Q]',Q))
+# print("{:<16} {}".format(empty, DASH*24))
