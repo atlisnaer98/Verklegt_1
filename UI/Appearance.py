01cbc932b91a99417e003e3b79dd417e7531064d
Atli Snær Jóhannsson <atlisjoh@gmail.com>
2019-12-05 10:41:13 +0000

    breyting í appearance

--- UI/Appearance.py
+++ UI/Appearance.py
@@ -7,30 +7,30 @@ LENGTH = 60
 # Role = "Select a Role"
 # option = "Select an option"
 M = "Menu"
-# D = "Destination"
+D = "Destination"
 # C = 'Create'
 # CH = 'Change info'
 # CV = 'Change voyage'
 # A = 'Airplanes'
 # AC = 'Assign crew'
-# E = "Employees"
-# V = "Voyage"
+E = "Employees"
+V = "Voyage"
 # S = "Show schedule "
-# L = "List employee"
+L = "List employee"
 # LD = 'List destination'
 # LV = 'list voyage'
 
 
-# B = "Back"
-# Q = "Quit"
+B = "Back"
+Q = "Quit"
 # empty = ''
 
 
-# print(DASH*52)
-# print("{:>26} ".format(M))
-# print(DASH*52)
-# print("{} {:<26}{} {} ".format('[1]', E, '[2]', V))
-# print("{} {:<26}{} {} ".format('[3]', D, '[4]', L))
+# print(DASH*LENGTH)
+# print("{:>30} ".format(M))
+# print(DASH*LENGTH)
+# print("{} {:<30}{} {} ".format('[1]', E, '[2]', V))
+# print("{} {:<30}{} {} ".format('[3]', D, '[4]', L))
 # print("{:<16} {}".format(empty, DASH*24))
 # print("{:<18} {} {} {} {} ".format(empty, '[B]',B, '[Q]',Q))
 # print("{:<16} {}".format(empty, DASH*24))
@@ -60,3 +60,8 @@ class Appearance:
         print(DASH*LENGTH)
         print("{:>30}".format(M))
         print(DASH*LENGTH)
+        print("{} {:<30}{} {} ".format('[1]', E, '[2]', V))
+        print("{} {:<30}{} {} ".format('[3]', D, '[4]', L))
+        #print("{:>30}".format(DASH*(LENGTH/2)))
+        #print("{:<30}  {} {} {} ".format('[B]',B, '[Q]',Q))
+        #print("{:<30}".format(DASH*LENGTH/2))
