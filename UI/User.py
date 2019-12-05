76487bea4c3808ce1232945c83936c249f8c1337
Gisli Thor <gislig18@ru.is>
2019-12-05 16:14:00 +0000

    What to change?

--- UI/User.py
+++ UI/User.py
@@ -85,7 +85,8 @@ class User:
             if action == lis[0]:
                 #changee = employee
                 print("{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(emp.get_ID_number(), emp.get_name(), emp.get_address(), emp.get_home_phone(), emp.get_mobile_number(), emp.get_email_address(), emp.get_job_title(), emp.get_rank(), emp.get_licence(), emp.get_activity()))
-                #self.ll.change_employee(emp)
+                changed = input("What do you want to change? ")
+                #self.ll.change_employee(emp,changed)
                 print("I liiike")
         # self.get_employee(action)
 
