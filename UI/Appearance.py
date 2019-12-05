719eb76fc87be2945551989af89d9535f674691f
Hjalti Sigurgeirsson <Hjalti@Hjaltis-MacBook-Air-3.local>
2019-12-05 15:41:21 +0000

    p

--- UI/Appearance.py
+++ UI/Appearance.py
@@ -10,7 +10,10 @@ M = "Menu"
 D = "Destination"
 C = 'Create'
 CH = 'Change info'
+CEI = 'Change Employee Info'
+CNE = 'Create new Employee'
 CND = 'Create new Destination'
+CNV = 'Create new Voyage'
 CV = 'Change voyage'
 A = 'Airplanes'
 AC = 'Assign crew'
@@ -96,13 +99,20 @@ class Appearance:
 
 
     def print_create_employee(self):
-        pass
+        print(DASH*LENGTH)
+        print("{:^60}".format(CNE))
+        print(DASH*LENGTH)
+        print("Please input necessary information:")
+        #Input upplýsingar
+        self.back_quit()
 
     def print_show_schedule(self):
         pass
     
     def print_change_employee_info(self):
-        pass
+        print(DASH*LENGTH)
+        print("{:^60}".format(CEI))
+        print(DASH*LENGTH)
 
     def print_employee_list(self):
         pass
@@ -120,8 +130,10 @@ class Appearance:
         print("{:^60}".format(CND))
         print(DASH*LENGTH)
         print("Please input necessary information:")
+        # Input Upplýsingar
+        self.back_quit()
 
-    
+
     def print_voyage_menu(self):
         print(DASH*LENGTH)
         print("{:^60}".format(V))
@@ -134,7 +146,13 @@ class Appearance:
         
 
     def print_add_voyage(self):
-        pass
+        print(DASH*LENGTH)
+        print("{:^60}".format(CNV))
+        print(DASH*LENGTH)
+        print("Please input necessary information:")
+        #Input upplýsingar
+        self.back_quit()
+
 
     def print_assign_crew(self):
         pass
