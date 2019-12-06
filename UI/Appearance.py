9e88536408fffa83d4b989b305e2266e9b3b185a
Gisli Thor <gislig18@ru.is>
2019-12-06 17:13:04 +0000

    Breyta má

--- UI/Appearance.py
+++ UI/Appearance.py
@@ -137,10 +137,6 @@ class Appearance:
         print("ID Number: {}\nName: {}\nAddress: {}\nHome phone: {}\nMobile number: {}\nEmail address: {}\nJob title: {}\nRank: {}\nLicence: {}\nActivity: {}\n"
         .format(emp.get_ID_number(), emp.get_name(), emp.get_address(), emp.get_home_phone(), emp.get_mobile_number(),
         emp.get_email_address(), emp.get_job_title(), emp.get_rank(), emp.get_licence(), emp.get_activity()))
-                
-
-               
-        
 
     def print_employee_list(self):
         pass
