8887bb0c4751d4854852600ccc649f33fa0ef27c
Gisli Thor <gislig18@ru.is>
2019-12-09 20:36:47 +0000

    Breyta aðeins í voyage repo

--- InformationLayerClasses/Voyage_repository.py
+++ InformationLayerClasses/Voyage_repository.py
@@ -4,35 +4,13 @@ import csv
 HEADER = "bookingReference,flightNumberAway,flightNumberHome,arrivingAt,departure,arrival,aircraftID,captain,copilot,fsm,fa1,fa2"
 class Voyage_repository:
 
-    def add_all_upcoming_flights(self,all_voyages,counter):
-        all_voyages_list = all_voyages
-        with open("./DATA/UpcomingFlights.csv","r",newline="") as all_flights:
-            reader = csv.DictReader(all_flights)
-            counter = counter
-            for line in reader:
-                if line["departingFrom"] == "Keflavik":
-                    counter +=1
-                    voyages = Voyage(counter,line["flightNumber"],line["flightNumber"],line["arrivingAt"],line["departure"],line["arrival"],line["aircraftID"],line["captain"],line["copilot"],line["fsm"],line["fa1"],line["fa2"])
-                    all_voyages_list.append(voyages)
-        return all_voyages_list
+    
 
     def update_voyage_file(self,voyage_list):
         with open("./DATA/Voyage.csv", "w+", newline="") as voyages:
             voyages.write("{}\n".format(HEADER))
             for voyage in voyage_list:
                 voyages.write("{}\n".format(str(voyage)))
-
-    def get_all_past_flights(self):
-        all_voyages_list = []
-        with open("./DATA/PastFlights.csv","r",newline="") as all_flights:
-            reader = csv.DictReader(all_flights)
-            counter = 0
-            for line in reader:
-                if line["departingFrom"] == "Keflavik":
-                    counter +=1
-                    voyages = Voyage(counter,line["flightNumber"],line["flightNumber"],line["arrivingAt"],line["departure"],line["arrival"],line["aircraftID"],line["captain"],line["copilot"],line["fsm"],line["fa1"],line["fa2"])
-                    all_voyages_list.append(voyages)
-        return all_voyages_list
 
     def get_all_voyages(self):
         """gets all the voyages"""
@@ -59,9 +37,26 @@ class Voyage_repository:
         a flight manager each cabin crew member can only go on one voyage each day'''
         pass
 
-    
-    
-    
+    def get_all_past_flights(self): #Was used to create the Voyage.csv file
+        all_voyages_list = []
+        with open("./DATA/PastFlights.csv","r",newline="") as all_flights:
+            reader = csv.DictReader(all_flights)
+            counter = 0
+            for line in reader:
+                if line["departingFrom"] == "Keflavik":
+                    counter +=1
+                    voyages = Voyage(counter,line["flightNumber"],line["flightNumber"],line["arrivingAt"],line["departure"],line["arrival"],line["aircraftID"],line["captain"],line["copilot"],line["fsm"],line["fa1"],line["fa2"])
+                    all_voyages_list.append(voyages)
+        return all_voyages_list
 
-
-    
+    def add_all_upcoming_flights(self,all_voyages,counter): #Was used to create the Voyage.csv file
+        all_voyages_list = all_voyages
+        with open("./DATA/UpcomingFlights.csv","r",newline="") as all_flights:
+            reader = csv.DictReader(all_flights)
+            counter = counter
+            for line in reader:
+                if line["departingFrom"] == "Keflavik":
+                    counter +=1
+                    voyages = Voyage(counter,line["flightNumber"],line["flightNumber"],line["arrivingAt"],line["departure"],line["arrival"],line["aircraftID"],line["captain"],line["copilot"],line["fsm"],line["fa1"],line["fa2"])
+                    all_voyages_list.append(voyages)
+        return all_voyages_list
