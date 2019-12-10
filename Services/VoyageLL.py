f6697d62bee5a4e9340759b57c994944d98ceefd
Gisli Thor <gislig18@ru.is>
2019-12-10 13:00:41 +0000

    list of voyages on a date

--- Services/VoyageLL.py
+++ Services/VoyageLL.py
@@ -1,6 +1,7 @@
 from Models.Voyage import Voyage
 import datetime
 import dateutil.parser
+from datetime import timedelta
 from InformationLayerClasses.API import Data_main
 class VoyageLL():
         
@@ -12,14 +13,14 @@ class VoyageLL():
         return all_voyage_list
 
 
-    def get_date_voyages(self, from_date):
+    def get_date_voyages(self, the_date):
         voyage_list = []
         all_voyage_list = self.dl.get_all_voyages()
-        parseddate = dateutil.parser.parse(from_date)
-        
+        from_date= dateutil.parser.parse(the_date)
+        to_date = from_date + timedelta(days=1)
         for voyage in all_voyage_list:
             parseddate =dateutil.parser.parse(voyage.get_departure())
-            if from_date <= parseddate and parseddate <= to_date:
+            if from_date <= parseddate and parseddate < to_date:
                 voyage_list.append(voyage)
         return voyage_list  
 
