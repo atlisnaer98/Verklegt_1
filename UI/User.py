771952d609b689cd4ffebe2006a73d62183c17ec
Gisli Thor <gislig18@ru.is>
2019-12-12 09:26:48 +0000

    user comment

--- UI/User.py
+++ UI/User.py
@@ -450,7 +450,7 @@ class User:
         voyage_list = self.ll.get_date_voyages(from_date,to_date)
         self.print_voyages_manned(voyage_list)
     
-    def get_voyages_for_timeperiod(self):
+    def get_voyages_for_timeperiod(self): #Þarf að vera valkostur fyrir þetta í apperance
         temp_date = input("Enter from date: YYYY-MM-DD:")                    
         from_date = dateutil.parser.parse(temp_date)
         temp_date = input("Enter to date: YYYY-MM-DD:")     
